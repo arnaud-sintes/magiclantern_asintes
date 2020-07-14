9ef6a93bc7edb3bbc303e45e68657a02b15e9759
danne <dudek53@gmail.com>
2020-07-14 11:09:13 +0200

    crop_rec.c:(5d3 expand no ratio anamorphic yet 10 units)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -1954,26 +1954,24 @@ static inline uint32_t reg_override_1x3(uint32_t reg, uint32_t old_val)
     
     switch (reg)
     {
-        {
-            case 0xC0F06804:
-                return (set_25fps && ratios) ? 0x8d6011b + reg_6804_width + (reg_6804_height << 16):
-            (set_25fps && !ratios) ? 0x8f8011b + reg_6804_width + (reg_6804_height << 16):
-            (!set_25fps && !ratios) ? 0x962011b + reg_6804_width + (reg_6804_height << 16):
-            0x93a011b + reg_6804_width + (reg_6804_height << 16);
-            case 0xC0F0713c:
-                return (set_25fps && ratios) ? 0x8da + reg_713c:
-            (set_25fps && !ratios) ? 0x8f8 + reg_713c:
-            (!set_25fps && !ratios) ? 0x962 + reg_713c:
-            0x93e + reg_713c;
-        }
+        case 0xC0F06804:
+            return (set_25fps && ratios) ? 0x8d6011b + reg_6804_width + (reg_6804_height << 16):
+        (set_25fps && !ratios) ? 0x8f8011b + reg_6804_width + (reg_6804_height << 16):
+        (!set_25fps && !ratios) ? 0x96c011b + reg_6804_width + (reg_6804_height << 16):
+        0x93a011b + reg_6804_width + (reg_6804_height << 16);
+        case 0xC0F0713c:
+            return (set_25fps && ratios) ? 0x8da + reg_713c:
+        (set_25fps && !ratios) ? 0x8f8 + reg_713c:
+        (!set_25fps && !ratios) ? 0x96c + reg_713c:
+        0x93e + reg_713c;
             
         case 0xC0F06014:
-            return set_25fps == 0x1 ? 0x9bd + reg_6014: 0xa27 + reg_6014;
+            return set_25fps ? 0x9ca + reg_6014: 0xa35 + reg_6014;
         case 0xC0F06008:
         case 0xC0F0600C:
-            return 0x1800180 + reg_6008 + (reg_6008 << 16);
+            return 0x17e017e + reg_6008 + (reg_6008 << 16);
         case 0xC0F06010:
-            return 0x180 + reg_6008;
+            return 0x17e + reg_6008;
     }
     
     return reg_override_bits(reg, old_val);
